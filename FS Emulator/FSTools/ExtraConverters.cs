221190326eb6abbdcd16c96958d3cf0ed843c8de
VictorGorban <VictorGorban2@ya.ru>
2018-11-27 20:18:06 +0300

    Testing creating files...

--- FS Emulator/FSTools/ExtraConverters.cs
+++ FS Emulator/FSTools/ExtraConverters.cs
@@ -53,7 +53,7 @@ namespace FS_Emulator.FSTools
 
 			if (path.Length == 0)
 			{
-				throw new ArgumentException("путь не может быть пустым",nameof(path));
+				return path;
 			}
 
 			if (path.Last() != '/')
